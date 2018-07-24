28b95ad1962e442f3a2065dfe2896c5c08473445
mudit jain <mudit.jain@lmsin.com>
2018-07-24 19:13:33 +0530

    commit project

---
+++ TestFramework/TestFramework/Service.swift
@@ -0,0 +1,23 @@
+//
+//  Service.swift
+//  TestFramework
+//
+//  Created by LMS on 24/07/18.
+//  Copyright © 2018 LMS. All rights reserved.
+//
+
+import Foundation
+
+public class Service{
+    
+    private init(){
+        
+    }
+    
+    public static func myFunction() -> String
+    {
+        return "My First framework is"
+    }
+    
+    
+}
